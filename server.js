53f6bd6853a94e9a9413936b66de0ac5edd4b2a7
Aazad1389a <hanifazad1389a@gmail.com>
2026-09-11 15:56:14 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
-const io = require('socket.io')(3000, {
-    cors: { origin: "*" }
-});
-
-const players = {};
+let bulletIdCounter = 0;
 
 io.on('connection', (socket) => {
-    console.log('Player connected:', socket.id);
-    
-    players[socket.id] = { x: (Math.random() - 0.5) * 20, z: (Math.random() - 0.5) * 20 };
-    
-    socket.emit('currentPlayers', players);
-    socket.broadcast.emit('currentPlayers', players);
+    // مقداردهی اولیه سلامت بازیکن
+    if (players[socket.id]) {
+        players[socket.id].health = 100;
+    }
 
-    socket.on('playerInput', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].z = data.z;
-            socket.broadcast.emit('playerMoved', { id: socket.id, x: data.x, z: data.z });
+    // مدیریت شلیک
+    socket.on('shootBullet', (data) => {
+        const bulletId = 'b_' + (bulletIdCounter++);
+        const bulletData = {
+            id: bulletId,
+            owner: socket.id,
+            x: data.x,
+            z: data.z,
+            dirX: data.dirX,
+            dirZ: data.dirZ
+        };
+        
+        // پخش شلیک به تمام بازیکنان آنلاین
+        io.emit('bulletFired', bulletData);
+    });
+
+    // مدیریت برخورد تیر به بازیکن
+    socket.on('bulletHitPlayer', (data) => {
+        const target = players[data.targetId];
+        if (target) {
+            target.health -= 20; // میزان آسیب هر تیر
+            
+            io.emit('playerHit', {
+                id: data.targetId,
+                health: target.health
+            });
         }
     });
 
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
+    // اسپاون مجدد پس از مرگ
+    socket.on('respawn', () => {
+        if (players[socket.id]) {
+            players[socket.id].health = 100;
+            players[socket.id].x = (Math.random() - 0.5) * 20;
+            players[socket.id].z = (Math.random() - 0.5) * 20;
+            
+            io.emit('playerHit', { id: socket.id, health: 100 });
+            io.emit('playerMoved', { id: socket.id, x: players[socket.id].x, z: players[socket.id].z });
+        }
     });
 });
